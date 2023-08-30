d357bd662319fb17207531be5ebc33ca1099070f
rivando al rasyid <119521535+rivando-al-rasyid@users.noreply.github.com>
2023-08-30 14:37:09 +0000

    tema

--- resources/views/admin/layouts/login.blade.php
+++ resources/views/admin/layouts/login.blade.php
@@ -12,14 +12,14 @@
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
     <!-- Nucleo Icons -->
-    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/nucleo-svg.css') }}" rel="stylesheet" />
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- CSS Files -->
-    <link id="pagestyle" href="{{ asset('assets/css/material-kit.css?v=3.0.4') }}" rel="stylesheet" /> <!-- Nepcha Analytics (nepcha.com) -->
+    <link id="pagestyle" href="{{ asset('assets/admin/css/material-kit.css?v=3.0.4') }}" rel="stylesheet" /> <!-- Nepcha Analytics (nepcha.com) -->
     <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
     <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
@@ -42,12 +42,12 @@
         @include('layouts.frontend.login')
     </div>
     <!--   Core JS Files   -->
-    <script src="{{ asset('assets/js/core/popper.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script> <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+    <script src="{{ asset('assets/admin/js/core/popper.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/admin/js/core/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/admin/js/plugins/perfect-scrollbar.min.js') }}"></script> <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
     <!--  Google Maps Plugin    -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
-    <script src="{{ asset('assets/js/material-kit.min.js?v=3.0.4') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/admin/js/material-kit.min.js?v=3.0.4') }}" type="text/javascript"></script>
 </body>
 
 </html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,22 +4,22 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/admin/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('assets/admin/img/favicon.png') }}">
     <title>
         Material Dashboard 2 by Creative Tim
     </title>
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
     <!-- Nucleo Icons -->
-    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/admin/css/nucleo-svg.css') }}" rel="stylesheet" />
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- CSS Files -->
-    <link id="pagestyle" href="{{ asset('assets/css/material-kit.css?v=3.1.0') }}" rel="stylesheet" />
+    <link id="pagestyle" href="{{ asset('assets/admin/css/material-kit.css?v=3.1.0') }}" rel="stylesheet" />
     <!-- Nepcha Analytics (nepcha.com) -->
     <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
     <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
@@ -42,10 +42,10 @@
         </div>
     </main>
     <!--   Core JS Files   -->
-    <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
-    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
-    <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/js/plugins/smooth-scrollbar.min.js') }}"></script>
     <script>
         var win = navigator.platform.indexOf('Win') > -1;
         if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -58,7 +58,7 @@
     </script>
     <!-- Github buttons -->
     <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="{{ asset('assets/js/material-kit.min.js?v=3.1.0') }}"></script>
+    <script src="{{ asset('assets/admin/js/material-kit.min.js?v=3.1.0') }}"></script>
 </body>
 
 </html>
