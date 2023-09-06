50464aa3bbc039581baf329125406bcc888a4f3b
rivando al rasyid <nougat-tutorial@gmail.com>
2023-09-06 18:35:51 +0700

    update

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,15 +15,11 @@ return [
     |
     */
 
-    'stateful' => explode(',', env(
-        'SANCTUM_STATEFUL_DOMAINS',
-        sprintf(
-            '%s%s',
-            'localhost,localhost:3000,142.132.150.169,142.132.150.169:8000,::1',
-            Sanctum::currentApplicationUrlWithPort()
-        )
-    )
-    ),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+        '%s%s',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        Sanctum::currentApplicationUrlWithPort()
+    ))),
 
     /*
     |--------------------------------------------------------------------------
